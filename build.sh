3855124931abe1977752cdf9c8ff967e139db026
Eric <50216183+ericsrodrigues@users.noreply.github.com>
2024-10-16 15:06:24 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,12 +12,58 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-rpm-ostree install screen
+### Third Party Repos
 
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
+# VSCode Repo
+
+tee /etc/yum.repos.d/vscode.repo <<'EOF'
+[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
+
+# Docker Repo
+
+tee /etc/yum.repos.d/docker-ce.repo <<'EOF'
+[docker-ce-stable]
+name=Docker CE Stable - $basearch
+baseurl=https://download.docker.com/linux/fedora/$releasever/$basearch/stable
+enabled=1
+gpgcheck=1
+gpgkey=https://download.docker.com/linux/fedora/gpg
+EOF
+
+# this installs a package from fedora repos
+rpm-ostree install \
+  code \
+  openssl \
+  zsh \
+  neovim \
+  vim \
+  gnome-tweaks \
+  docker-ce \
+  docker-ce-cli \
+  containerd.io \
+  docker-buildx-plugin \
+  docker-compose-plugin \
+  bootc \
+  rclone \
+  fastfetch \
+  gnome-shell-extension-appindicator \
+  nautilus-gsconnect \
+  gnome-shell-extension-appindicator \
+  jetbrains-mono-fonts \
+  google-noto-sans-cjk-fonts \
+  gh
+
+# uninstall packages
+rpm-ostree uninstall \
+  firefox \
+  firefox-langpacks
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+systemctl enable podman.socket docker.service
