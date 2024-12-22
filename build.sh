ee0b80a6f06e6b8ef8e344c20442a64066170ca0
Eric <50216183+ericsrodrigues@users.noreply.github.com>
2024-12-22 01:44:26 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -25,20 +25,6 @@ gpgcheck=1
 gpgkey=https://packages.microsoft.com/keys/microsoft.asc
 EOF
 
-# SwayNC Repo
-tee /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:erikreider:SwayNotificationCenter.repo <<'EOF'
-[copr:copr.fedorainfracloud.org:erikreider:SwayNotificationCenter]
-name=Copr repo for SwayNotificationCenter owned by Erik Reider
-baseurl=https://download.copr.fedorainfracloud.org/results/erikreider/SwayNotificationCenter/fedora-$releasever-$basearch/
-type=rpm-md
-skip_if_unavailable=True
-gpgcheck=1
-gpgkey=https://download.copr.fedorainfracloud.org/results/erikreider/SwayNotificationCenter/pubkey.gpg
-repo_gpgcheck=0
-enabled=1
-enabled_metadata=1
-EOF
-
 # Docker Repo
 
 tee /etc/yum.repos.d/docker-ce.repo <<'EOF'
@@ -53,23 +39,17 @@ EOF
 # this installs a package from fedora repos
 rpm-ostree install \
 code \
-zsh \
 neovim \
 vim \
-gnome-tweaks \
 docker-ce \
 docker-ce-cli \
 containerd.io \
 docker-buildx-plugin \
 docker-compose-plugin \
 bootc \
-rclone \
 fastfetch \
 jetbrains-mono-fonts \
 google-noto-sans-cjk-fonts \
-SwayNotificationCenter \
-alacritty \
-gammastep \
 gh \
 corectrl
 
